fe44517022520c9f6c571005aeb2480114a303cc
Gilberto.oliveira <gilberto.oliveira@qitech.com.br>
2026-09-01 13:29:51 -0300

    .

--- database/database.sql
+++ database/database.sql
@@ -6,18 +6,18 @@ CREATE TABLE sample_entity_status(
 );
 
 INSERT INTO sample_entity_status (enumerator) VALUES
-	 ('created'),
-     ('pending'),
-	 ('success'),
-     ('failed');
+('created'),
+('pending'),
+('success'),
+('failed');
 
 CREATE TABLE sample_entity(
-    id                     SERIAL PRIMARY KEY,
-    sample_entity_key      CHAR(36) NOT NULL,
-    status_id              INTEGER NOT NULL REFERENCES sample_entity_status(id),
-    sample_entity_data     JSONB NOT NULL,
-    counter                INTEGER NOT NULL,
-    created_at             TIMESTAMP NOT NULL DEFAULT(NOW()),
+    id                              SERIAL PRIMARY KEY,
+    sample_entity_key               CHAR(36) NOT NULL,
+    status_id                       INTEGER NOT NULL REFERENCES sample_entity_status(id),
+    sample_entity_data              JSONB NOT NULL,
+    counter                         INTEGER NOT NULL,
+    created_at                      TIMESTAMP NOT NULL DEFAULT(NOW()),
     UNIQUE(sample_entity_key)
 );
 
